e9b897240fa289613305c902299f0edb295684a8
flak <ali76034@gmail.com>
2018-09-21 21:41:00 -0400

    added some placeholders

---
+++ check_hands.py
@@ -0,0 +1,13 @@
+def n_of_a_kind(list, n):
+    """
+    :param list: List of card objects
+    :param n: The number of cards that should be of the same type. 2 is a pair. 4 is four of a kind.
+    :return: The number of n of a kinds which occur in the hand. 22445 has two pairs.
+    """
+
+
+def sequence_of_n_length(list, n):
+    pass
+
+def n_of_a_suit(list, n):
+    pass
